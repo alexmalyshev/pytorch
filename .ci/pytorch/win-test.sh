04e7ccb287c562fa4a8400c1ddd13c7c551f74a2
atalman <atalman@fb.com>
2025-07-25 12:48:53 -0700

    wintests

--- .ci/pytorch/win-test.sh
+++ .ci/pytorch/win-test.sh
@@ -55,6 +55,8 @@ python -m pip install pulp==2.9.0
 # Install expecttest to merge https://github.com/pytorch/pytorch/pull/155308
 python -m pip install expecttest==0.3.0
 
+python -m pip install -r $SCRIPT_PARENT_DIR/../docker/requirements-ci.txt
+
 run_tests() {
     # Run nvidia-smi if available
     for path in '/c/Program Files/NVIDIA Corporation/NVSMI/nvidia-smi.exe' /c/Windows/System32/nvidia-smi.exe; do
